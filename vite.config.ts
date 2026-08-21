9126d6ece2f8d77e5e4a2c4073c83ca9a00adb2b
Mehak Pathania <pathaniamehak07@gmail.com>
2026-08-21 17:59:47 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base:'/local-link/',
+    base:'/Local-link/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
